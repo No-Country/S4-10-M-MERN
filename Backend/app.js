b10416566d6f7b8fad37df617a740c15730d668a
requin883 <requin883@gmail.com>
2022-09-28 09:09:40 -0700

    feat: add some imports and use helmet and CORS

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,12 @@
-// Back
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+app.use(helmet());
