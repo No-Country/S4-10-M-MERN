5e3bf9452825996f2fe740dd3a01d958cc968a67
requin883 <requin883@gmail.com>
2022-09-28 09:10:27 -0700

    Add user and movie models

---
+++ Backend/src/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose, { mongo } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    score: {
+        type: Array,
+        default: []
+    }
+});
+
+userSchema.statics.encryptPassword = async (password) => {
+    try {
+        return await bcrypt.hash(password, 10)
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+userSchema.statics.comparePassword = async (password, passwordToCompare) => {
+    return await bcrypt.compare(password, passwordToCompare);
+};
+
+export default new mongoose.model('users', userSchema);
