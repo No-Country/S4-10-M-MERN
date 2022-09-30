200d8847998069d5e913cf324ff4b767c1cfb148
requin883 <requin883@gmail.com>
2022-09-30 07:41:52 -0700

    hotfix: remove app files

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+const PORT = 8080 || 5000;
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
-    credentials: true
+    credentials: true,
 }));
 
 app.use(helmet());
+
+app.listen(PORT, (err) => {
+    !err ? console.log("server on PORT", PORT) : console.log(err);
+})
