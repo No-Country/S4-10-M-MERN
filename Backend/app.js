47a2b25f8d987af04fe9881830cfe318dfb4283a
Jakiro12 <eclipsisnocte@gmail.com>
2022-09-30 20:25:29 -0300

    cors config

--- Backend/app.js
+++ Backend/app.js
@@ -5,10 +5,12 @@ const PORT = 8080 || 5000;
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+const urlsAllowed = ['aqui van las url que hacen peticion desde el front', 'ejemplo', 'http://localhost:4000']
+const methodsRequired=['GET','POST'] // por el momento necesitamos estos dos
+
+app.get('/rutaEjemplo',cors({credentials:true, methods:methodsRequired[0],origin:urlsAllowed }),(req,res)=>{
+    res.send('limitaciones con cors')
+})
 
 app.use(helmet());
 
