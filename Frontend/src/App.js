a6b88b51f559d49f2fd1e1e48b0e169bc576cabe
“clambeta” <claudiambetam@gmail.com>
2022-10-02 20:44:37 -0500

    Login y Register básico

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { Login } from "../src/components/login/index";
+import { Register } from "../src/components/register/index";
+import React, { useState } from "react";
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
-    <BrowserRouter>
+   <div>
+       <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>path /</h1>} />
         <Route path="/home" element={<h1>path /home</h1>} />
       </Routes>
     </BrowserRouter>
+
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+   </div>
+   
   );
 }
 
