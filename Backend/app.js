9ee9fc8d695d4831e55ce6977b2127b572c48413
Luca Hardmeier <hardmeierluca@gmail.com>
2022-10-03 12:47:37 -0300

    User route and controller added

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { } from "dotenv";
+import userRouter from "./src/routes/user.routes";
+
 
 const PORT = 8080 || 5000;
 
@@ -10,11 +12,11 @@ const app = express();
 const urlsAllowed = ['aqui van las url que hacen peticion desde el front', 'ejemplo', 'http://localhost:4000']
 const methodsRequired = ['GET', 'POST'] // por el momento necesitamos estos dos
 
-app.get('/rutaEjemplo', cors({ credentials: true, methods: methodsRequired[0], origin: urlsAllowed }), (req, res) => {
-    res.send('limitaciones con cors')
-})
+const cors = cors({ credentials: true, methods: methodsRequired[0], origin: urlsAllowed })
 
 app.use(helmet());
+
+app.use('/api/v1/user', cors, userRouter)
 
 app.listen(PORT, (err) => {
     !err ? console.log("server on PORT", PORT) : console.log(err);
