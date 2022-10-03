18da53852150d778d0096d82b0a4ec5467d23ced
Jakiro12 <eclipsisnocte@gmail.com>
2022-10-03 17:52:55 -0300

    limitations to handle cors from backend

--- Backend/app.js
+++ Backend/app.js
@@ -3,20 +3,18 @@ import cors from "cors";
 import helmet from "helmet";
 import { } from "dotenv";
 import userRouter from "./src/routes/user.routes";
-
+import { corsPermissions } from "./src/middlewares/corsConfig";
 
 const PORT = 8080 || 5000;
 
 const app = express();
 
-const urlsAllowed = ['aqui van las url que hacen peticion desde el front', 'ejemplo', 'http://localhost:4000']
-const methodsRequired = ['GET', 'POST'] // por el momento necesitamos estos dos
 
-const cors = cors({ credentials: true, methods: methodsRequired[0], origin: urlsAllowed })
+const cors = cors({corsPermissions})
 
 app.use(helmet());
 
-app.use('/api/v1/user', cors, userRouter)
+app.use('/api/v1/user', cors(), userRouter)
 
 app.listen(PORT, (err) => {
     !err ? console.log("server on PORT", PORT) : console.log(err);
