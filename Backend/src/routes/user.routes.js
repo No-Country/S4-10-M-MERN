a09c22b3382b7ec7d5031cbaa06c31a6563a9220
requin883 <requin883@gmail.com>
2022-10-09 16:01:22 -0700

    add routes to get, update, and delete user

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { createUser } from '../controllers/user.controller.js'
+import { createUser, findUserByCategory, updateUserByCategory, deleteExistingUser } from '../controllers/user.controller.js'
 const userRouter = express.Router()
 
-userRouter.post('/', createUser)
+userRouter.post('/', createUser);
 
+userRouter.get('/getUserInfo', findUserByCategory);
+
+userRouter.put('/updateUserInfo', updateUserByCategory);
+
+userRouter.delete('/deleteUser', deleteExistingUser);
 
 
 export default userRouter
