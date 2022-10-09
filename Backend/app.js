f77f24b984a5d87232e82efbfc1dede29fbc6cdd
requin883 <requin883@gmail.com>
2022-10-09 15:59:05 -0700

    add JSON bodyparser

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,19 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 const app = express();
 import helmet from "helmet";
 import cors from "cors";
-import { corsPermissions } from "./src/middlewares/corsConfig.js"
+import { corsPermissions } from "./src/middlewares/corsConfig.js";
 import './src/config/db.js';
-import userRouter from './src/routes/user.routes.js'
+import userRouter from './src/routes/user.routes.js';
 import movieRouter from './src/routes/movie.routes.js';
+import bodyParser from 'body-parser';
 
 app.use(cors({ corsPermissions }))
 app.use(helmet());
+
+app.use(bodyParser.json());
 
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/movie', movieRouter)
