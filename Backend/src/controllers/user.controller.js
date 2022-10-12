800c66f1f1f1013e541b7b596b1314adc4f7f4cc
requin883 <requin883@gmail.com>
2022-10-12 09:07:29 -0700

    Change createUser with registerUser and create loginUser function

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,9 @@
-import userClass from "../utils/userClass.js"
 
-export const createUser = async (req, res) => {
+import userClass from "../utils/userClass.js"
+import userModel from "../models/userModel.js";
+import jwt from 'jsonwebtoken';
+
+export const registerUser = async (req, res) => {
     try {
         const user = await userClass.createNewUser(req.body);
         if (!user) return res.status(400).send('Mail de usuario ya existente');
@@ -10,9 +13,38 @@ export const createUser = async (req, res) => {
     }
 }
 
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const userExists = await userClass.findByEmail(email);
+
+        if (!userExists) return res.status(500).send("El usuario o contraseña son incorrectos");
+
+        const passwordMatch = await userModel.comparePassword(password, userExists.password);
+
+        console.log(passwordMatch);
+
+        if (!passwordMatch) return res.status(500).send("El usuario o contraseña son incorrectos");
+
+        const authToken = jwt.sign(
+            { id: userExists._id },
+            process.env.SECRET_ACCESS_KEY,
+            { expiresIn: '5m' }
+        )
+        return res.status(200).send({
+            message: "usuario logeado",
+            authToken: loginToken
+        });
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
 export const findUserById = async (req, res) => {
     try {
-        const foundUser = await userClass.findById(req.body);
+        const foundUser = await userClass.findById(req.query);
         return foundUser ? res.status(200).send(foundUser) : res.status(400).send("Usuario no encontrado");
     } catch (err) {
         res.status(500).send(err.message);
@@ -32,7 +64,7 @@ export const updateUserByCategory = async (req, res) => {
 export const deleteExistingUser = async (req, res) => {
     try {
         const deletedUser = await userClass.deleteUser(req.body);
-        return deletedUser ? res.status(200).send("Usuario eliminado"):res.status(400).send("No se ha podido encontrar el usuario");
+        return deletedUser ? res.status(200).send("Usuario eliminado") : res.status(400).send("No se ha podido encontrar el usuario");
     } catch (err) {
         console.log(err.message);
     }
