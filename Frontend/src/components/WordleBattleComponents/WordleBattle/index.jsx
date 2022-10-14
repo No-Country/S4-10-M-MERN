168eb0d090f9adaae91462d5538978dfedbec4cc
David Emiliano Morales <105241040+DEMAcomputacion@users.noreply.github.com>
2022-10-13 22:20:59 -0300

    Se agrega el componente WordleBattle

---
+++ Frontend/src/components/WordleBattleComponents/WordleBattle/index.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { client } from "../SocketIo/index.js";
+import Keypad from "../../WordleComponents/Keypad";
+import { keys } from "../../WordleComponents/Wordle/keys.js";
+import useWordle from "../../../hooks/useWordle.js";
+import Grid from "../../WordleComponents/Grid";
+import RemoteGrid from "../RemoteGrid/index.jsx";
+
+import "./index.css";
+
+
+function WordleBattle() {
+  const location = useLocation();
+  const solution = "pater"
+  const [turn, setTurn] = useState(0);
+  const [remotePlay, setRemotePlay] = useState([...Array(6)]);
+  const { currentGuess, guesses, isCorrect, usedKeys, handleKeyup } = useWordle(
+    solution,
+    turn,
+    setTurn
+  );
+
+
+  useEffect(()=>{
+    client.on("newPlay", (play) => {
+      setRemotePlay(play)
+    });
+    return () => {
+      client.on("newPlay", (play) => {
+        setRemotePlay(play)
+      });
+    };
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup]);
+
+  useEffect(() => {
+      client.emit(
+        "newPlay",location.state.contrincante, guesses,
+        (res) => {
+          console.log(res.status + " ok");
+        }
+      );
+  }, [guesses]);
+
+  //Envia una jugada al contrincantre
+
+  return !isCorrect ? (
+    <div className="radialBackground">
+      <div className="generalText">Solución - {solution}</div>
+      <div className="generalText">Actual jugada - {currentGuess}</div>
+      <div className="container">
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <RemoteGrid guesses={remotePlay}/>
+      </div>
+
+      <Keypad keys={keys} usedKeys={usedKeys} />
+      {turn >= 6 ? (
+        <h1 style={{ color: "red" }}>
+          No te quedan turnos, la solución era{" "}
+          <span style={{ color: "#f00", fontSize: 42 }}>{solution}</span>
+        </h1>
+      ) : (
+        ""
+      )}
+    </div>
+  ) : (
+    <div>Ganaste</div>
+  );
+};
+
+export default WordleBattle;
---
+++ Frontend/src/components/WordleBattleComponents/WordleBattlePrepare/index.jsx
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { client } from "../SocketIo";
+
+import "./index.css";
+
+
+function WordleBattlePrepare() {
+  const navigate = useNavigate();
+  const [myId, setMyId] = useState(client.id);
+  const [opponent, setOpponent] = useState("");
+  const [state, setState] = useState("");
+  const [copiedId, setCopiedId] = useState(false);
+
+  useEffect(() => {
+    const stateAcceptGame = () => {
+      setState(`Te han invitado a jugar una prtida`);
+    }
+
+    const stateStartGame = (player) => {
+      setOpponent(player);
+      setState("Partida aceptada, abriendo el Juego");
+      setTimeout(() => startGame(), 1000);
+    }
+
+    client.on("acceptGame", stateAcceptGame);
+    client.on("startGame", stateStartGame);
+
+    return () => {
+      client.off("acceptGame", stateAcceptGame);
+      client.off("startGame", stateStartGame);
+    };
+  });
+
+  const saveOpponentId = (e) => {
+    setOpponent(e.target.value);
+  };
+
+  const sendId = (e) => {
+    e.preventDefault();
+    if (opponent === myId) {
+      setState("No puedes introducir tu propio ID#");
+    } else {
+      client.emit(
+        "prepareGame",
+        {
+          transmitter: myId,
+          opponent: opponent,
+        },
+        (response) => {
+          setState(`Esperando a que el contrincante acepte la partida`);
+        }
+      );
+    }
+  };
+
+  const acceptGame = () => {
+    client.emit("gameAccepted", (res) => {
+      setState("Has aceptado la partida, abriendo el juego");
+      setOpponent(res.player);
+      setTimeout(() => startGame(res.player), 1000);
+    });
+  };
+
+  const startGame = (opponent2) => {
+    navigate("/wordle-battle/game", {
+      state: { id: myId, opponent, opponent2 },
+    });
+  };
+
+  const copyIdToClipboard = () => {
+    navigator.clipboard.writeText(myId);
+    setCopiedId(true);
+  };
+
+  return (
+    <div className="game-container">
+      <div className="recuadro">
+        <h4 className="generalText">MI ID#:</h4>
+        <div className="space">
+          <div className="idToClipboardText">{myId}</div>
+          <div
+            className={
+              copiedId ? "idToClipboardIconVisited" : "idToClipboardIcon"
+            }
+            onClick={copyIdToClipboard}
+          >
+            <img
+              src={require("./copy-icon.png")}
+              className="copy-icon"
+              alt="Copy to clipboard"
+            />
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={acceptGame}
+        className={
+          state === `Te han invitado a jugar una prtida`
+            ? "botonAceptar"
+            : "hidden"
+        }
+      >
+        ACEPTAR PARTIDA
+      </button>
+      <div className="recuadro">
+        <h4 className="generalText">ID# CONTRINCANTE:</h4>
+        <div className="space">
+          <form onSubmit={sendId}>
+            <input
+              id="idContrincante"
+              className="inputIdContrincante"
+              onChange={saveOpponentId}
+              type="text"
+            />
+            <input
+              type="submit"
+              className="buttonIdContrincante"
+              id="enviador"
+              value="INVITAR"
+            />
+          </form>
+        </div>
+      </div>
+      <p className="generalText">Estado: </p>
+      <p className="estado">{state}</p>
+    </div>
+  );
+}
+
+export default WordleBattlePrepare;
