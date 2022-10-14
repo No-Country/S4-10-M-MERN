0a3b6bbbfb8901bd6c85c551fb9ce75918c3d828
Luca Hardmeier <hardmeierluca@gmail.com>
2022-10-14 12:08:58 -0300

    character and wordle routers added to app.js

--- Backend/app.js
+++ Backend/app.js
@@ -8,13 +8,17 @@ import { corsPermissions } from "./src/middlewares/corsConfig.js"
 import './src/config/db.js';
 import userRouter from './src/routes/user.routes.js'
 import movieRouter from './src/routes/movie.routes.js';
-import {socketIoServer} from "./src/socket.io/server.js";
+import characterRouter from './src/routes/character.routes.js';
+import wordleRouter from './src/routes/wordle.routes.js';
+import { socketIoServer } from "./src/socket.io/server.js";
 
 app.use(cors({ corsPermissions }))
 app.use(helmet());
 
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/movie', movieRouter)
+app.use('/api/v1/character', characterRouter)
+app.use('/api/v1/word', wordleRouter)
 
 socketIoServer()
 
