b0535ab6fda1165bd95bf31fd1117418d5a115d1
RichardIrala <richardirala47@hotmail.com>
2022-10-19 18:45:47 -0300

    Merge en el index

--- Backend/index.js
+++ Backend/index.js
@@ -4,10 +4,6 @@ import express from "express";
 const app = express();
 import helmet from "helmet";
 import cors from "cors";
-<<<<<<< HEAD
-import { corsPermissions } from "./src/middlewares/corsConfig.js";
-=======
->>>>>>> 1cb0f0bc94aec0e1e1de8677ac7f1b3f547d2a10
 import './src/config/db.js';
 import userRouter from './src/routes/user.routes.js';
 import movieRouter from './src/routes/movie.routes.js';
@@ -17,11 +13,7 @@ import wordleRouter from './src/routes/wordle.routes.js';
 import { socketIoServer } from "./src/socket.io/server.js";
 
 app.use(cors())
-<<<<<<< HEAD
-// app.use(helmet());
-=======
 app.use(helmet());
->>>>>>> 1cb0f0bc94aec0e1e1de8677ac7f1b3f547d2a10
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
